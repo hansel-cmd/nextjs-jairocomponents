0b7941d177191fd4334b410155e65a0fee3ecbaa
Hansel <valkirecesa@gmail.com>
2022-01-31 21:13:54 +0800

    added route links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,30 @@
 import styles from '../styles/Navbar.module.css'
 
+import { useRouter } from "next/router"
+
 const Navbar = () => {
+
+    const router = useRouter();
+    
+    const links = [
+        {
+            route: '/', // home
+            is_active: router.pathname == '/' ? styles['navbar-link-active'] : '',
+        }, 
+        {
+            route: '/listing', // find a job
+            is_active: router.pathname == '/listing' ? styles['navbar-link-active'] : '',
+        },
+        {
+            route: '/companies', // companies
+            is_active: '',
+        },
+        {
+            route: '/post a job', // post a job
+            is_active: '',
+        }
+    ]
+    
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
@@ -14,13 +38,20 @@ const Navbar = () => {
 
                 <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul className={`navbar-nav mb-2 mb-lg-0 ${styles['navbar-ul']}`}>
-                        <li className={`nav-item ${styles['hover-bottom-border']}`}>
-                            <a className="nav-link active" href="#">Find a job</a>
+                        <li className={
+                            `nav-item ${styles['hover-bottom-border']} 
+                            ${links[1]['is_active']}`
+                        }>
+                            <a className="nav-link" href="#">Find a job</a>
                         </li>
-                        <li className={`nav-item ${styles['hover-bottom-border']}`}>
+                        <li className={`nav-item ${styles['hover-bottom-border']}
+                            ${links[2]['is_active']}
+                        `}>
                             <a className="nav-link" href="#">Companies</a>
                         </li>
-                        <li className={`nav-item ${styles['hover-bottom-border']}`}>
+                        <li className={`nav-item ${styles['hover-bottom-border']}
+                            ${links[3]['is_active']}
+                        `}>
                             <a className="nav-link" href="#">Post a Job</a>
                         </li>
                     </ul>
