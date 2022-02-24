c70ea91168fa64f7ea19760d08baf444d137dd05
Hansel <valkirecesa@gmail.com>
2022-02-24 13:18:57 +0800

    added post a job page

---
+++ pages/post-job.js
@@ -0,0 +1,42 @@
+const PostJob = () => {
+
+
+
+
+
+
+
+    return (
+        <section className="text-dark px-lg-0 p-lg-0 pt-lg-5">
+
+            <div className="container px-0 pt-5 pt-lg-0 mt-5">
+
+                <div className="row mt-5 pt-5 d-flex flex-column justify-content-center align-items-center align-items-center">
+
+                    <h3 className="fw-bold text-center">
+                        Welcome to <span className="text-info">Jairo Jobs</span> for Employers!
+                    </h3>
+                    <p className="pt-3 custom-max-width text-center">Build your business with us. Work with our certified and experienced consultants, architects, and developers. We go above and beyond what's expected and will bring incredible high value to your business.</p>
+
+                    <button className="btn btn-info w-25 mt-2">Get Started</button>
+                </div>
+            </div>
+
+
+            <div className="container px-0 pt-5 pt-lg-0 mt-5">
+
+                <div className="row mt-5 pt-5 d-flex flex-column justify-content-center align-items-center align-items-center">
+
+                    <h3 className="fw-bold text-center">
+                        Welcome to <span className="text-info">Jairo Jobs</span> for Employers!
+                    </h3>
+                    <p className="pt-3 custom-max-width text-center">Build your business with us. Work with our certified and experienced consultants, architects, and developers. We go above and beyond what's expected and will bring incredible high value to your business.</p>
+
+                    <button className="btn btn-info w-25 mt-2">Get Started</button>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default PostJob;
