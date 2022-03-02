3c6acbcbc638c0ed2a6cc0d6462e9cf6770e7201
Hansel <valkirecesa@gmail.com>
2022-03-02 20:04:41 +0800

    added sign-up page

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,38 +1,31 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
-const SignIn = () => {
+const SignUp = () => {
 
-    const bg_url = '/bg_img.jpg'
+    const bg_url = '/bg_img1.jpg'
     const bg_property = {
         'background': `url(${bg_url}) no-repeat`,
         'backgroundSize': 'cover',
         'backgroundClip': 'initial',
         'backgroundOrigin': 'initial',
-        'boxSizing':  'border-box'
+        'boxSizing': 'border-box'
     }
 
     return (
         <div style={bg_property} className="vh-100 py-5 py-lg-0 pt-lg-5">
             <div className="container">
-                <div className="row">
+                <div className="row flex-row-reverse">
                     <div className="col-md-1 d-none d-lg-block"></div>
                     <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                        <div className="px-4 card rounded-5 mb-4 shadow ">
-                            <div className="card-header no-border-bottom bg-transparent pt-4 px-4 pb-0">
-                                <div className="row">
-                                    <div className="col-12">
-                                        <h5 className="text-info">
-                                            <span className="navbar-brand">
-                                                <img src="/logo.png" alt="" width="200" height="84" />
-                                            </span>
-                                        </h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card-body px-4 pt-0">
+                        <div className="px-4 card rounded-5 mb-4 shadow">
+                            <div className="card-body px-4 pt-4 mt-3">
 
-                                <div className="pb-3">
-                                    <h2 className="fw-bold">Welcome Back!</h2>
+                                <div className="pb-1">
+                                    <h3 className="fw-bold lh-1 mb-1">Welcome to <br/><span className="text-info">Jairo Jobs</span></h3>
+
+                                    <p className="text-secondary">
+                                        Contact us now. We will surely get back to you as soon as possible.
+                                    </p>
                                 </div>
 
                                 {/* Email Address */}
@@ -47,28 +40,34 @@ const SignIn = () => {
                                     <input type="password" className="form-control shadow-sm" id="password" placeholder="Enter your password..." />
                                 </div>
 
-                                {/* Checkbox: keep me sign in */}
-                                <div className="form-check pb-3">
-                                    <input className="form-check-input" type="checkbox" id="keep_me_sign_in" />
-                                    <label className="form-check-label" htmlFor="keep_me_sign_in">
-                                        Keep me sign in
-                                    </label>
+                                {/* Confirm Password */}
+                                <div className="pb-3">
+                                    <label htmlFor="confirm_password" className="form-label">Password</label>
+                                    <input type="password" className="form-control shadow-sm" id="confirm_password" placeholder="Enter your password again..." />
                                 </div>
 
-                                {/* sign in button */}
+                                {/* 1st filter option */}
+                                <div className="pb-3">
+
+                                    <span className="no-background text-start">
+                                        Purpose
+                                    </span>
+
+                                    <select id="industry" className="form-select mt-2 shadow-sm" role="button" aria-label="industry">
+                                        <option defaultValue>Default</option>
+                                        <option value="x">Lorem, ipsum dolor.</option>
+                                        <option value="y">Lorem, ipsum.</option>
+                                        <option value="z">Lorem ipsum dolor sit.</option>
+                                    </select>
+                                </div>
+
+                                {/* create account button */}
                                 <div className="row mb-4">
                                     <div className="col d-grid d-block">
                                         <Link href={`/view-company-jobs/[indicate_company_name]`}>
-                                            <button type="button" className="btn btn-secondary">Sign In</button>
+                                            <button type="button" className="btn btn-secondary">Create an Account</button>
                                         </Link>
                                     </div>
-                                </div>
-
-                                {/* Forgot Password */}
-                                <div className="row pb-3">
-                                    <Link href="/">
-                                        <a className="text-center" style={{ 'textDecoration': 'none' }}>Forgot Password?</a>
-                                    </Link>
                                 </div>
 
                                 {/* ---OR--- */}
@@ -95,15 +94,15 @@ const SignIn = () => {
                                     </div>
                                 </div>
 
-                                <div className="row pb-3">
-                                    <h4 className="fw-bold mb-0 text-center">New to Jairo Jobs?</h4>
-                                    <h4 className="fw-bold mb-0 text-center">Join Us!</h4>
-                                </div>
-
-                                <div className="row pb-3">
-                                    <Link href="/sign-up">
-                                        <a className="text-center h5">Create an Account</a>
-                                    </Link>
+                                <div className="row pb-3 text-center">
+                                    <p>
+                                        Have an account?
+                                        <span className="px-2">
+                                            <Link href="/sign-in">
+                                                <a className="fw-bold">Sign In</a>
+                                            </Link>
+                                        </span>
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -114,4 +113,4 @@ const SignIn = () => {
     );
 }
 
-export default SignIn;
+export default SignUp;
