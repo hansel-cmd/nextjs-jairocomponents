24298dffa1e589f7d6ef7faca88428d5f1660991
Hansel <valkirecesa@gmail.com>
2022-04-01 18:27:38 +0800

    updated long card styling

--- components/LongCard.js
+++ components/LongCard.js
@@ -1,26 +1,37 @@
-const LongCard = ({details,setJob}) => {
+const LongCard = ({ details, setJob }) => {
     return (
-        <div className="card rounded-5 border-highlight-left mb-4 shadow" role="button" onClick={setJob}>
-            <div className="card-header no-border-bottom bg-transparent pt-4 px-4">
-                <h5 className="text-info">{details.title}</h5>
-                <p className="fs-6">{details.company}</p>
-            </div>
-            <div className="card-body">
-
+        <div className="card rounded-5 bg-outline-primary-opacity-5 primary-hover mb-4 shadow" role="button" onClick={setJob}>
+            <div className="card-header no-border-bottom bg-transparent pt-4 px-4 pb-0">
                 <div className="row">
-                    <div className="col-md-6">
-                        <p><span className="pe-2"><i className="bi bi-gift"></i></span>{details.workLocation}</p>
-                    </div>
-                    <div className="col-md-6">
-                        <p><span className="pe-2"><i className="bi bi-gift"></i></span>{details.companyLocation}</p>
+                    <div className="col-12 d-flex align-items-center justify-content-between">
+                        <h5 className="text-info">
+                            <span className="navbar-brand">
+                                <img src="/logo2.png" alt="" width="50" height="50" />
+                            </span>
+
+                        </h5>
+                        <h5 className="text-info fw-bold">
+                            P25,000 - P30,000
+                        </h5>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-6">
-                        <p><span className="pe-2"><i className="bi bi-gift"></i></span>{details.companyLocation}</p>
+            </div>
+            <div className="card-body pt-0 px-4">
+                <p className="fs-7 mb-2 text-secondary fw-bold">{details.company}</p>
+                <h5 className="text-dark fw-bold">{details.title}</h5>
+                <div className="pb-2">
+                    <button className="badge bg-outline-primary-opacity-20 rounded-pill me-2">Full Time</button>
+                    <button className="badge bg-outline-info-opacity-20 rounded-pill me-2">Remote (Temp)</button>
+                </div>
+                <p className="text-dark">{details.companyLocation}</p>
+                <p className="fs-7 text-secondary">March 20, 2022</p>
+                <hr />
+                <div className="row align-items-center">
+                    <div className="col">
+                        <button type="button" className="btn btn-info px-4 rounded" data-bs-toggle="modal" data-bs-target="#apply_now_modal">Apply</button>
                     </div>
-                    <div className="col-md-6">
-                        <p><span className="pe-2"><i className="bi bi-gift"></i></span>{details.industry}</p>
+                    <div className="col-6">
+                        <p className="fs-7 text-secondary text-end mb-0">9 replies</p>
                     </div>
                 </div>
             </div>
