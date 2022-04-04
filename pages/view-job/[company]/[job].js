92c2fbfa2fc3316f8d3d0243bb3f8c24afd44b29
Hansel <valkirecesa@gmail.com>
2022-04-04 22:16:17 +0800

    changed job details styling and structure

--- pages/view-job/[company]/[job].js
+++ pages/view-job/[company]/[job].js
@@ -1,147 +1,131 @@
-import CompanyCardDetails from "../../../components/CompanyCardDetails"
-import ApplyModal from "../../../components/ApplyModal"
-
 const JobPage = () => {
     return (
-        <section className="text-dark px-lg-0 p-lg-0 pt-lg-5">
+        <section className="text-dark px-lg-0 p-lg-0 pt-lg-5 mt-5">
 
             <div className="container px-0 pt-5 pt-lg-0">
-
-                {/* Banner */}
-                <div className="row">
-                    <div className="col px-4">
-
-                        {/* Job Name and Apply Button */}
-                        <div className="card rounded-5 border-highlight-left mb-4 shadow" >
-                            <div className="card-header no-border-bottom bg-transparent py-3 px-4">
-                                <div className="row p-2 align-items-center">
-                                    <div className="col-md-8 col-lg-10 d-flex flex-wrap align-items-center pb-3 pb-md-0">
-                                        <h3 className="text-dark mb-0 fw-bold">
-                                            .NET Developer
-                                        </h3>
-                                    </div>
-                                    <div className="col-md-4 col-lg-2">
-                                        <button type="button" className="btn btn-info rounded-4 w-100 p-2" data-bs-toggle="modal" data-bs-target="#apply_now_modal">Apply Now</button>
-                                    </div>
-                                </div>
-                            </div>
+                <div className="card-header no-border-bottom bg-transparent pt-4 pb-0 px-4">
+                    <div className="row align-items-center pb-3">
+                        <div className="col-10 col-sm-8">
+                            <h4 className="text-dark mb-0">
+                                <span className="navbar-brand">
+                                    <img src="/logo2.png" alt="" width="64" height="64" />
+                                </span>
+                                Jairosoft Inc.
+                            </h4>
                         </div>
+                        <div className="col-2 col-sm-4 d-flex justify-content-end align-items-start">
+                            <span className='fs-7 text-dark'>March 20, 2022</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-10 col-sm-8">
+                            <h3 className="text-info fw-bold">.NET Developer</h3>
+                            <p className="fs-6 text-dark">Davao City and Cebu City, Philippines</p>
+                        </div>
+                        <div className="col-10 col-sm-4">
+                            <p className="fs-7 text-dark text-end" style={{ backgroundColor: "1px solid black !important" }}>
+                                <i className="bi bi-share px-1 rounded" role="button" style={{ border: "1px solid #C4C4C4", color: "#C4C4C4" }}></i>
+                            </p>
+                            <p className="fs-7 text-dark text-end">Applicants: 9</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-body px-4">
+                    {/* icons with label */}
+                    <div className="row pb-4 px-2 mb-4">
+                        <div className="col-4 bg-primary px-4 p-3" style={{ borderTopLeftRadius: "1rem", borderBottomLeftRadius: "1rem" }}>
+                            <p className="text-white mb-0">Type</p>
+                            <p className="text-white fw-bold mb-0">Full Time</p>
+                        </div>
+                        <div className="col-4 bg-secondary px-4 p-3">
+                            <p className="text-white mb-0">Work Set-up</p>
+                            <p className="text-white fw-bold mb-0">Remote (Temporary)</p>
+                        </div>
+                        <div className="col-4 bg-info px-4 p-3" style={{ borderTopRightRadius: "1rem", borderBottomRightRadius: "1rem" }}>
+                            <p className="text-white mb-0">Salary</p>
+                            <p className="text-white fw-bold mb-0">P25,000 - P30,000</p>
+                        </div>
+                    </div>
 
+                    {/* Position Description */}
+                    <div className="pb-3 pt-4 mt-5">
+                        <h6 className="text-primary fw-bold text-uppercase">Position Description:</h6>
+                        <p className="text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eius tempore ad debitis ipsa impedit omnis aliquam id. Architecto voluptate quidem, perferendis minus sint illum illo, dolor, harum voluptas veniam ratione totam exercitationem. Quos, nam. Iste ipsum velit voluptas distinctio...</p>
+                    </div>
+
+                    {/* Job Responsibilities */}
+                    <div className="pb-3 pt-4">
+                        <h6 className="text-primary fw-bold text-uppercase">Job Responsibilities:</h6>
+                        <ul className="text-dark list-unstyled">
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur deserunt, laboriosam labore odit maiores atque?
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos asperiores repudiandae commodi fuga! Distinctio nemo iure iusto nihil unde sunt.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere impedit, aut harum, architecto vero qui soluta, placeat odio ut nihil quisquam repellendus recusandae.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptas ipsa velit dolor aliquam quo cum quae minima!
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis et cupiditate nihil dignissimos repellat!
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, dicta placeat.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, debitis quod quam amet accusamus temporibus!
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* Qualifications */}
+                    <div className="pb-3 pt-4">
+                        <h6 className="text-primary fw-bold text-uppercase">Qualifications:</h6>
+                        <ul className="text-dark list-unstyled">
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptas ipsa velit dolor aliquam quo cum quae minima!
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere impedit, aut harum, architecto vero qui soluta, placeat odio ut nihil quisquam repellendus recusandae.
+                            </li>
+                            <li>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nam laborum. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="row mb-4 gx-4 mt-5">
+                        <div className="col">
+                            <button type="button" className="btn btn-primary px-4 py-2 me-4">Apply Now</button>
+                            <button type="button" className="btn btn-primary px-4 py-2">View Company</button>
+                        </div>
                     </div>
                 </div>
 
-                {/* Content */}
-                <div className="row pb-4">
-
-                    {/* left: Description */}
-                    <div className='col-lg-8 px-4'>
-
-                        {/* Company Details */}
-                        <div className="pt-0">
-
-                            <div className="px-md-2 pt-4">
-
-                                {/* Icons and details */}
-                                <div className="pb-4">
-                                    <p className="mb-0">
-                                        <span className="pe-2">
-                                            <i className="bi bi-gift"></i>
-                                        </span>
-                                        <span className="fw-bold">30 </span>
-                                        Applied
-                                    </p>
-                                    <p className="mb-0">
-                                        <span className="pe-2">
-                                            <i className="bi bi-gift"></i>
-                                        </span>
-                                        Remote (Temporary)
-                                    </p>
-                                    <p className="mb-0">
-                                        <span className="pe-2">
-                                            <i className="bi bi-gift"></i>
-                                        </span>
-                                        Full Time
-                                    </p>
-                                    <p className="mb-0">
-                                        <span className="pe-2">
-                                            <i className="bi bi-gift"></i>
-                                        </span>
-                                        Davao or Cebu City, Philippines
-                                    </p>
-                                </div>
-
-
-                                {/* Description */}
-                                <div className="py-3">
-                                    <h6>Position Description:</h6>
-                                    <p className="text-secondary text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat, rem sed quam dolorem ab laborum blanditiis cum delectus iusto explicabo, magnam voluptatibus? Enim cum facere dolore, eaque reiciendis, adipisci repellat repellendus beatae sequi suscipit rem voluptas at eum accusamus quasi tempora doloremque laborum veniam distinctio soluta ipsum nulla dolores? Totam.</p>
-                                </div>
-
-                                {/* Bullets */}
-                                <div className="pb-3">
-                                    <h6>Job Duties & Responsibilities:</h6>
-                                    <ul className="text-secondary">
-                                        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium optio nesciunt vero quas dignissimos saepe odit, nam odio distinctio animi.</li>
-                                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eos cum voluptatem omnis? Quos, aspernatur!</li>
-                                        <li>Lorem ipsum dolor sit.</li>
-                                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-                                    </ul>
-                                </div>
-
-                                {/* Bullets */}
-                                <div className="pb-3">
-                                    <h6>Skills:</h6>
-                                    <ul className="text-secondary">
-                                        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium optio nesciunt vero quas dignissimos saepe odit, nam odio distinctio animi.</li>
-                                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eos cum voluptatem omnis? Quos, aspernatur!</li>
-                                        <li>Lorem ipsum dolor sit.</li>
-                                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-                                    </ul>
-                                </div>
-
-                                {/* Bullets */}
-                                <div className="pb-3">
-                                    <h6>Qualitifactions and Related Experience:</h6>
-                                    <ul className="text-secondary">
-                                        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium optio nesciunt vero quas dignissimos saepe odit, nam odio distinctio animi.</li>
-                                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eos cum voluptatem omnis? Quos, aspernatur!</li>
-                                        <li>Lorem ipsum dolor sit.</li>
-                                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-                                    </ul>
-                                </div>
-
-                                {/* Bullets */}
-                                <div className="pb-3">
-                                    <h6>ASP.NET, VB.NET, C#:</h6>
-                                    <ul className="text-secondary">
-                                        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium optio nesciunt vero quas dignissimos saepe odit, nam odio distinctio animi.</li>
-                                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eos cum voluptatem omnis? Quos, aspernatur!</li>
-                                        <li>Lorem ipsum dolor sit.</li>
-                                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-                                    </ul>
-                                </div>
-
-                                <button type="button" className="btn btn-info rounded-4 px-4" data-bs-toggle="modal" data-bs-target="#apply_now_modal">Apply Now</button>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* right: Cards */}
-                    <div className="col-lg-4 p-4">
-
-                        <div className="card rounded-5 mb-4 shadow border border-info">
-                            <img src="/test.jpg" className="card-img-top rounded-5" />
-                        </div>
-
-                        <CompanyCardDetails></CompanyCardDetails>
-                    </div>
-
-                </div>
-                
-                
-                {/* Apply Modal */}
-                <ApplyModal></ApplyModal>
             </div>
         </section>
     );
